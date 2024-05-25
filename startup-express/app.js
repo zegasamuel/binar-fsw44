a971bd30f5efcf00618d76722a0fb060df0345f7
Samuel Zega <samuelzega@Samuels-MacBook-Pro.local>
2024-05-25 12:03:27 +0700

    add ejs

--- startup-express/app.js
+++ startup-express/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
+var productRouter = require('./routes/product')
 
 const pgsql = require('knex')({
     client: 'pg',
@@ -32,7 +33,7 @@ var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -41,6 +42,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+app.use('/product', productRouter )
 app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
