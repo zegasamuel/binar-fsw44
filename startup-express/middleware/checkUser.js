de3857ec1478ee240d2985c22cea8bf86987da0b
Samuel Zega <samuelzega@Samuels-MacBook-Pro.local>
2024-05-28 20:41:37 +0700

    update;

---
+++ startup-express/middleware/checkUser.js
@@ -0,0 +1,21 @@
+const knex = require('knex')(require('../knexfile').development)
+module.exports = class {
+    static checkIfUserExist = async (req, res, next) => {
+        const {email} = req.body;
+        const user = await knex('users').where('email', email)
+        if (user.length ==0) {
+            next()
+        }else {
+            res.json({status: 400, message: 'Email already exist'})
+        }
+    }
+    static checkIsAdmin = async (req, res, next) => {
+        const {email} = req.body;
+        const user = await knex('users').where('email', email)
+        if (user.length ==0) {
+            next()
+        }else {
+            res.json({status: 400, message: 'Email already exist'})
+        }
+    }
+}
--- startup-express/routes/users.js
+++ startup-express/routes/users.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 const userController = require('../controller/userController')
+const checkUserMiddleware = require('../middleware/checkUser')
 
 /* GET users listing. */
-router.get('/', userController.getAllUser);
-router.post('/', userController.addUser)
-module.exports = router;
+
+router.get('/createuser', function(req, res, next) {
+    res.render('createuser', { title: 'Create User' });
+  });
+  
+router.get('/', userController.getAllUser)
+router.post('/', checkUserMiddleware.checkIfUserExist, userController.addUser)
+module.exports = router
