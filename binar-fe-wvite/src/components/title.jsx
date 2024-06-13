41d99a1522d88d97b22a881953725c73d8b36a08
Samuel Zega <samuelzega@Samuels-MacBook-Pro.local>
2024-06-13 20:31:50 +0700

    menambahkan materi tentang react

---
+++ binar-fe-wvite/src/components/title.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Title({title}) {
+  return (
+    <div>{title} Vite</div>
+  )
+}
---
+++ binar-fe-wvite/src/components/viteLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import viteLogo from '/vite.svg'
+
+export default function ViteLogo() {
+    return (
+        <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+    )
+}
